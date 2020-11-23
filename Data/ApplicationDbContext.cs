99b293183d6dea2f52302ce9f35acd9b0aa0ff6f
Hau Nguyen <huutho1999@gmail.com>
2020-11-23 18:20:50 +0700

    Create new project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CellphoneStore.Models;
+
+namespace CellphoneStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Models/Supplier.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CellphoneStore.Models
+{
+    public class Supplier
+    {
+
+    }
+}
